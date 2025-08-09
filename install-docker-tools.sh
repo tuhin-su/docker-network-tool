56d13c9a5b640177cfc1592768ea79e63171453c
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-08-09 23:28:17 +0530

    final update

--- install-docker-tools.sh
+++ install-docker-tools.sh
@@ -5,6 +5,7 @@ BIN_DIR="/usr/local/bin"
 
 sudo systemctl disable docker.socket --now
 sudo systemctl disable docker --now
+
 echo "[+] Copying docker-up..."
 sudo cp docker-up $BIN_DIR/docker-up
 
@@ -16,6 +17,9 @@ sudo chmod +x $BIN_DIR/docker-up $BIN_DIR/docker-down
 
 echo "[+] Disabling Docker auto-start at boot..."
 sudo systemctl disable docker docker.socket
+
+echo "[+] Installing Network tool..."
+sudo ./setup.sh
 
 echo "[+] Installation complete!"
 echo "    Use: docker-up   → to start Docker"
